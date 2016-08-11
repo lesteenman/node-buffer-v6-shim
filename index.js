e06df21480b00b600f80e966c6810c0e94aa5143
AJ ONeal <awesome@coolaj86.com>
2016-08-10 19:20:29 -0600

    try harder to make Buffer.from work

--- index.js
+++ index.js
@@ -25,3 +25,15 @@ if (!Buffer.allocUnsafeSlow) {
 if (!Buffer.from) {
   Buffer.from = newBuffer;
 }
+
+try {
+  Buffer.from('1337', 'hex');
+} catch(e) {
+  // wish I could do something here to fix the broken Buffer.from
+  try {
+    Buffer.from = newBuffer;
+  } catch(e) {
+    // but alas, I cannot
+    console.warn("Your node version has buggy Buffer.from support. Please update to node >= v4.5 or >= v6.3");
+  }
+}
